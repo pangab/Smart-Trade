3f88a584bf3722939b75315846be717e0384520c
pangab <gabrielpanagiotidis@Gabriels-MacBook-Pro-2.local>
2014-11-13 13:06:52 +0000

    Initial Commit

---
+++ SmartTrade/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartTrade
+//
+//  Created by Gabriel Panagiotidis on 13/11/2014.
+//  Copyright (c) 2014 Gab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartTrade/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartTrade
+//
+//  Created by Gabriel Panagiotidis on 13/11/2014.
+//  Copyright (c) 2014 Gab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
